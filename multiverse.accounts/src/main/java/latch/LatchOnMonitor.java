59b01bce75ba33636ad35edaee88173eb7b32947
sleyzerzon <senya22@yahoo.com>
2016-09-17 14:11:48 -0400

    countdown latch

---
+++ multiverse.accounts/src/main/java/latch/LatchOnMonitor.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package latch;
+
+/**
+ * @author Simeon
+ *
+ */
+public class LatchOnMonitor {
+
+	private boolean open = false;
+	
+	public synchronized void doOpen() {
+		open = true;
+		this.notifyAll();
+	}
+	
+	public synchronized void await() throws InterruptedException{
+		while(!open) {
+			this.wait();
+		}
+	}
+}
